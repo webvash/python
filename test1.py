0b67b5a6bef728088ce555e7e6141cf823dc55ec
siyavash Rahimi <siavashelc@gmail.com>
2025-03-24 09:30:12 +0100

    start-1

--- test1.py
+++ test1.py
@@ -6,4 +6,17 @@ print("Hello"+" Siyavash")
 print("Hello"+input("what is your name?"))
 '''
 
-print(len(input("Input:")))
+#print(len(input("Input:")))
+
+##########start-1
+a=input("a:")
+b=input("b:")
+
+c=a
+a=b
+b=c
+
+print("a="+a)
+print("b="+b)
+
+
