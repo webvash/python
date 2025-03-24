1d1082d03dea150a113b0ac1d4c983c3fbba55b0
siyavash Rahimi <siavashelc@gmail.com>
2025-03-24 10:53:05 +0100

    start-Band Name Generator

--- test1.py
+++ test1.py
@@ -9,6 +9,7 @@ print("Hello"+input("what is your name?"))
 #print(len(input("Input:")))
 
 ##########start-1
+"""
 a=input("a:")
 b=input("b:")
 
@@ -18,5 +19,12 @@ b=c
 
 print("a="+a)
 print("b="+b)
+"""
+##########start-Band Name Generator
+city=input("please enter tha name of city:\n")
+name=input("what is your dog name:\n")
+print (city+" "+name)
+
+
 
 
